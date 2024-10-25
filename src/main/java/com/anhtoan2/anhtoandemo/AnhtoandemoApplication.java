932c77decb1fe7190ea5be74f17e1b3c5ad6313a
toanjr <bilong2468@gmail.com>
2024-10-25 16:02:05 +0700

    ta
    ta

---
+++ src/main/java/com/anhtoan2/anhtoandemo/AnhtoandemoApplication.java
@@ -0,0 +1,13 @@
+package com.anhtoan2.anhtoandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnhtoandemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnhtoandemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhtoan2/anhtoandemo/AnhtoandemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhtoan2.anhtoandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnhtoandemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
